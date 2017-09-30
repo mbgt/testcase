b69c6cd6943a5f39d2fe6e717b0618ae27bed8f4
Matthias Baumgartner <mbaumgar@gmx.ch>
2017-09-30 17:48:40 +0200

    paramtrisierter Test

--- src/main/java/ch/mab/tc/Testcase.java
+++ src/main/java/ch/mab/tc/Testcase.java
@@ -7,17 +7,36 @@ package ch.mab.tc;
 
 import ch.mab.tc.jaxb.InkassoFallType;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 /**
  *
  * @author mab
  */
 public class Testcase {
 
-    private InkassoFallType inkassoFall;
+    private String resource;
 
-    public Testcase(InkassoFallType inkassoFall) {
-        this.inkassoFall = inkassoFall;
+    public Testcase(String resource) {
+        this.resource = resource;
     }
 
+    public void execute() throws Exception {
+        setup();
+    }
+
+    private InkassoFallType load() throws Exception {
+        InputStream is =  new BufferedInputStream(new FileInputStream(resource));
+
+        Marshalling marshalling = new Marshalling();
+
+        return marshalling.unmarhall(is);
+    }
+
+    private void setup() throws Exception {
+        load();
+    }
 
 }
--- src/test/java/ch/mab/tc/TestcaseTest.java
+++ src/test/java/ch/mab/tc/TestcaseTest.java
@@ -1,26 +1,43 @@
 package ch.mab.tc;
 
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import javax.xml.bind.JAXBException;
-import java.io.InputStream;
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 
 /**
  * Created by mab on 30.09.17.
  */
+@RunWith(Parameterized.class)
 public class TestcaseTest {
 
+    @Parameterized.Parameters(name="{0}")
+    public static Collection<String> loadTests() {
+
+        URL rootUrl = Testcase.class.getResource("/");
+
+        File[] files = new File(rootUrl.getPath()).listFiles();
+
+        return files !=null ?Arrays.stream(files) //
+                .map(File::getPath) //
+                .filter(s->s.endsWith(".xml")) //
+                .collect(Collectors.toList()): Collections.emptyList();
+    }
+
+    @Parameterized.Parameter
+    public String resource;
+
     @Test
-    public void load() throws JAXBException {
+    public void load() throws Exception {
 
-        InputStream is = getClass().getResourceAsStream("/esrzahlung.xml");
-
-        Marshalling marshalling = new Marshalling();
-
-        Object unmarhall = marshalling.unmarhall(is);
-
-        Assert.assertNotNull(unmarhall);
+        Testcase testcase = new Testcase(resource);
+        testcase.execute();
     }
 }
